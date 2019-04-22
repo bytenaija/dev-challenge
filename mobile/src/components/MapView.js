d851804c1981aa343e4ee210e8830f4726c4968c
Everistus Olumese <everistusolumese@gmail.com>
2019-04-22 09:38:27 +0100

    Feature: Added the display of list of companies and one selected company. Also integrated location display for users and companies

--- mobile/src/components/MapView.js
+++ mobile/src/components/MapView.js
@@ -6,7 +6,7 @@ export default ({ longitude, latitude }) => {
   const DEFAULT_URL = `<div style="width: 100%"><iframe width="100%" height="200" src="https://maps.google.com/maps?width=100%&amp;height=200&amp;hl=en&amp;q=${latitude},${longitude}&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><br />`;
 
   return (
-    <View style={{ width: '100%', height: 200 }}>
+    <View style={{ width: '100%', height: 100 }}>
       <WebView
         automaticallyAdjustContentInsets={false}
         source={{
