3cc9c12a8bd90f844529cee3bded4688fc70fb88
Everistus Olumese <everistusolumese@gmail.com>
2019-04-22 09:39:35 +0100

    Final Cleanup and testing

--- server/src/graphql/resolvers/Mutation/updateUser.js
+++ server/src/graphql/resolvers/Mutation/updateUser.js
@@ -1,17 +1,18 @@
-import { setUser, getUser } from '../../../helpers';
+import { setUser, getUser, isBase64 } from '../../../helpers';
+
+const base16 = require('atob');
 
 export default async function User(root, { user }, { ctx }, info) {
-  // todo: 1 this throws a unfriendly (and potentially unsafe) error if a non-existnant user ID is entered.
-  // how can we check for a non-existing user id and throw a more friendly error.
-
-  // todo: 2 why is this update overwriting existing user data? Need to fix this so that just data input is
-  // updated rather than overwritting all the data.
+  console.log(user);
   try {
-    const isUserExists = await getUser(user.id);
-
+    const id = isBase64(user.id) ? base16(user.id) : user.id;
+    console.log(id);
+    const isUserExists = await getUser(id);
+    console.log(isUserExists);
     if (isUserExists) {
+      user = { ...user, id };
       const updatedUserInformation = { ...isUserExists, ...user };
-
+      console.log(updatedUserInformation);
       await setUser(updatedUserInformation);
       return true;
     }
--- server/src/graphql/resolvers/Query/users.js
+++ server/src/graphql/resolvers/Query/users.js
@@ -6,5 +6,6 @@ import { fetchUsers } from '../../../helpers';
 
 export default async function users(root, { filters }, { ctx }, info) {
   const filteredUsers = fetchUsers(filters);
+  console.log(await filteredUsers)
   return filteredUsers;
 }
